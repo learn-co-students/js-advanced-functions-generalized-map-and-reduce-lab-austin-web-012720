5759f4352dee6713634037867c83fb92b6ba8f55
qudroturkey <reesemb1@gmail.com>
2020-03-17 16:17:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(array, cb) {
+  const r = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const theElement = array[i];
+    r.push(cb(theElement));
+  }
+
+  return r;
+}
+
+function reduce(array, cb, starting) {
+  let r = (starting) || array[0];
+  let i = (starting) ? 0 : 1;
+
+  for (; i < array.length; i++) {
+    r = cb(array[i], r);
+  }
+
+  return r;
+}
