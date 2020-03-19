0283f1f8fb46eb69d561063133225d0f92931d84
github username <github email address>
2020-03-19 17:42:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(array, callBackFunction) {
+  const newArray = [];
+  for(let i = 0; i < array.length; i ++) {
+    newArray.push(callBackFunction(array[i]));    
+  }
+  return newArray;
+}
+
+function reduce(array, callBackFunction, initialValue) {
+  let initVal = (!!initialValue) ? initialValue : array[0];
+  let i = (!!initialValue) ? 0 : 1;
+  
+  for(; i < array.length; i++) {
+    initVal = callBackFunction(array[i], initVal);
+  }
+  return initVal;
+} 
